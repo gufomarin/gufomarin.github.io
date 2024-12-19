f246e22521d6b15a81a11250f966b69ebd16f78e
Gabriel Marin <gbrlmarn@gmail.com>
2024-12-19 18:45:38 +0200

    build.sh, resources: added personal website

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Update CV
-(cd ./resources/rendercv; rendercv render Gabriel_Marin_CV.yaml || exit 1)
+(cd ./resources/rendercv || exit 1; rendercv render Gabriel_Marin_CV.yaml || exit 1)
 cp "./resources/rendercv/rendercv_output/Gabriel_Marin_CV.pdf" "./content/about/"
 
 # Update Hugo Site
